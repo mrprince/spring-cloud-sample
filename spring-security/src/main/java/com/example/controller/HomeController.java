050f5d9a22509a3fa522f3bd46598cf336bdfb2b
mrprince <mrprince@qq.com>
2016-12-30 11:47:09 +0800

    add translation

--- spring-security/src/main/java/com/example/controller/HomeController.java
+++ spring-security/src/main/java/com/example/controller/HomeController.java
@@ -3,8 +3,6 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -20,16 +18,8 @@ public class HomeController {
     }
 
     @RequestMapping("/login")
-    public ModelAndView getLoginForm(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "logout", required = false) String logout) {
-
-        String message = "";
-        if (error != null) {
-            message = "Incorrect username or password !";
-        } else if (logout != null) {
-            message = "Logout successful !";
-        }
-        return new ModelAndView("login", "message", message);
+    public String getLoginForm() {
+        return "login";
     }
-
 
 }
--- spring-security/src/main/java/com/example/security/SecurityConfig.java
+++ spring-security/src/main/java/com/example/security/SecurityConfig.java
@@ -2,19 +2,15 @@ package com.example.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/h2-console/**", "/webjars/**", "/css/**", "/js/**");
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/js/**", "/css/**").permitAll()
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin()

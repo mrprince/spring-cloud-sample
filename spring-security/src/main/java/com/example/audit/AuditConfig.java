2f93408d0f2fd2e58db59a5a93cb051de6843a02
mrprince <mrprince@qq.com>
2017-01-24 17:51:31 +0800

    add audit4j configuration file

--- spring-security/src/main/java/com/example/audit/AuditConfig.java
+++ spring-security/src/main/java/com/example/audit/AuditConfig.java
@@ -22,30 +22,4 @@ public class AuditConfig {
         return auditAspect;
     }
 
-    // If you want to load configurations from file
-    // (resources/audit4j.conf.yaml),
-    // comment below method
-//    @Bean
-//    public DatabaseAuditHandler databaseHandler() {
-//        DatabaseAuditHandler dbHandler = new DatabaseAuditHandler();
-//        dbHandler.setEmbedded("true");
-//        return dbHandler;
-//    }
-
-    // If you want to load configurations from file
-    // (resources/audit4j.conf.yaml),
-    // comment below method
-    @Bean
-    public SpringAudit4jConfig springAudit4jConfig() {
-        SpringAudit4jConfig springAudit4jConfig = new SpringAudit4jConfig();
-        springAudit4jConfig.setLayout(new SimpleLayout());
-        List<Handler> handlers = new ArrayList<Handler>();
-        handlers.add(new ConsoleAuditHandler());
-
-        //handlers.add(databaseHandler());
-        springAudit4jConfig.setHandlers(handlers);
-        springAudit4jConfig.setMetaData(new AuditMetaData());
-        return springAudit4jConfig;
-    }
-
 }

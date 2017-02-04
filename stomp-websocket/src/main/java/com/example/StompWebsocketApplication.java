72668f1b729d66eab485417b03f7beaf16f2d2ae
mrprince <mrprince@qq.com>
2017-02-05 00:11:09 +0800

    add stomp websocket

---
+++ stomp-websocket/src/main/java/com/example/StompWebsocketApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StompWebsocketApplication.class, args);
+	}
+}
